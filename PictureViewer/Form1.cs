3795394aadafecfeb1407c944466c92c56cead06
satoshi246ss <satoshi246ss@yahoo.co.jp>
2016-01-24 23:06:44 +0900

    UDP受信ルーチン　127.0.0.1追加

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -275,8 +275,7 @@ namespace MT3
             {
                 udp_packet_id++;
                 byte[] rcvBytes = udpc.Receive(ref remoteEP);
-                if (rcvBytes.Length == size_kd && remoteEP.Address.ToString() == mmFsiKV1000)
-                //if (rcvBytes.Length == size_kd && (remoteEP.Address.ToString() == mmFsiKV1000 || remoteEP.Address.ToString() == "127.0.0.1"))
+                if (rcvBytes.Length == size_kd && (remoteEP.Address.ToString() == mmFsiKV1000 || remoteEP.Address.ToString() == "127.0.0.1"))
                 {
                     kd = ToStruct1(rcvBytes);
                     bw.ReportProgress(0, kd);
